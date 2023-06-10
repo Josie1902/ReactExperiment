24c6ac83659ab50b5d5cb010932f868119c3e245
Josephine Tan Xin Yi <josephine_tan@mymail.sutd.edu.sg>
2023-06-10 23:23:18 +0800

    my first react app

---
+++ react-next13-tinker/app/about/loading.js
@@ -0,0 +1,4 @@
+export default function Loading() {
+    // You can add any UI inside Loading, including a Skeleton.
+    <h1>The page is loading ...</h1>
+}
---
+++ react-next13-tinker/app/about/page.js
@@ -0,0 +1,13 @@
+export const metadata = {
+    title: 'About',
+}
+
+export default function About(){
+    // throw new Error("failed to load data")
+    return(
+        <div>
+            <h1>About Page</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut beatae quidem vero ipsum ea ullam, eum eius provident velit! Qui, perferendis. Dignissimos tenetur debitis id veritatis laborum, ipsam minima quia?</p>
+        </div>
+    )
+}
