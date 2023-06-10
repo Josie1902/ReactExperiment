efee3a36dda17dbad27f90308fa5ed9b05d30eaf
Josephine Tan Xin Yi <josephine_tan@mymail.sutd.edu.sg>
2023-06-11 00:00:06 +0800

    navigating pages

--- react-next13-tinker/app/about/page.js
+++ react-next13-tinker/app/about/page.js
@@ -8,7 +8,7 @@ export default function About(){
         <div>
             <h1 className="text-lg py-2 m-4">About Page</h1>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut beatae quidem vero ipsum ea ullam, eum eius provident velit! Qui, perferendis. Dignissimos tenetur debitis id veritatis laborum, ipsam minima quia?</p>
-            <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">Button</button>
+            <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded m-2">Button</button>
         </div>
     )
 }
