b9d7399ef002a485a2abfd52a858dc49f696da53
Josephine Tan Xin Yi <josephine_tan@mymail.sutd.edu.sg>
2023-06-11 01:21:17 +0800

    async await movie api

--- react-next13-tinker/app/page.js
+++ react-next13-tinker/app/page.js
@@ -1,7 +1,24 @@
-export default function Home() {
+import Movie from "./movie"
+
+export default async function Home() {
+  const data = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`)
+  const res = await data.json()
+  // console.log(res)
+
   return (
     <main>
       <h1>Hello World</h1>
+      {res.results.map(movie => (
+        <div>
+          <Movie
+            key = {movie.id}
+            id = {movie.id}
+            title = {movie.title}
+            poster_path = {movie.poster_path}
+            release_date = {movie.release_date}
+          />
+        </div>
+      ))}
     </main>
   )
 }
