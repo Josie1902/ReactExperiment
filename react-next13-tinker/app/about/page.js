362ca78646d117060fbc0549474694642834b47e
Josephine Tan Xin Yi <josephine_tan@mymail.sutd.edu.sg>
2023-06-10 23:35:01 +0800

    added tailwind style

--- react-next13-tinker/app/about/page.js
+++ react-next13-tinker/app/about/page.js
@@ -6,8 +6,9 @@ export default function About(){
     // throw new Error("failed to load data")
     return(
         <div>
-            <h1>About Page</h1>
+            <h1 className="text-lg py-2 m-4">About Page</h1>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut beatae quidem vero ipsum ea ullam, eum eius provident velit! Qui, perferendis. Dignissimos tenetur debitis id veritatis laborum, ipsam minima quia?</p>
+            <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">Button</button>
         </div>
     )
 }
