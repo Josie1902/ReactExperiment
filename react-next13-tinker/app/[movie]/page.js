88f9e85cdb1f2665b3cc57160cfb204f5b888ac0
Josephine Tan Xin Yi <josephine_tan@mymail.sutd.edu.sg>
2023-06-11 16:58:33 +0800

    understanding revalidate data

--- react-next13-tinker/app/[movie]/page.js
+++ react-next13-tinker/app/[movie]/page.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default async function MovieDetail({params}){
     const {movie} = params
     const imagePath = "https://image.tmdb.org/t/p/original/"
-    const data = await fetch(`https://api.themoviedb.org/3/movie/${movie}?api_key=${process.env.API_KEY}`)
+    const data = await fetch(`https://api.themoviedb.org/3/movie/${movie}?api_key=${process.env.API_KEY}`,{next:{revalidate:0}})
     const res = await data.json()
     // console.log(res)
     return(
