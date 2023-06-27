52f040a1c53d093981070f167d8086ecb956fead
Josephine Tan Xin Yi <josephine_tan@mymail.sutd.edu.sg>
2023-06-28 00:16:47 +0800

    a small mysel nextjs project

---
+++ my-sql/app/page.js
@@ -0,0 +1,60 @@
+"use client"
+
+import { useState } from 'react';
+
+const Home = () => {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await fetch('/api/form', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, email }),
+      });
+
+      if (response.ok) {
+        console.log('User added successfully.');
+      } else {
+        throw new Error('Error adding user.');
+      }
+    } catch (error) {
+      console.error('Error adding user:', error);
+    }
+
+    setUsername('');
+    setEmail('');
+  };
+
+  return (
+    <div>
+      <h1>Add User</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Username:</label>
+          <input type="text" value={username} onChange={handleUsernameChange} />
+        </div>
+        <div>
+          <label>Email:</label>
+          <input type="email" value={email} onChange={handleEmailChange} />
+        </div>
+        <button type="submit">Add User</button>
+      </form>
+    </div>
+  );
+};
+
+export default Home;
